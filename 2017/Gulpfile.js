3af7f983ea04b17a3cd2a0556e5ed3d95a08ab81
Laurent Paoletti <lp@providenz.fr>
2017-01-11 22:38:23 +0100

    Add css postprocessing tools

--- 2017/Gulpfile.js
+++ 2017/Gulpfile.js
@@ -1,26 +1,31 @@
 var gulp = require('gulp');
 var gutil = require("gulp-util");
 var sass = require('gulp-sass');
+var plumber = require("gulp-plumber");
+var autoprefixer = require("gulp-autoprefixer");
+var csso = require("gulp-csso");
+
 
 // Watching watching scss/html files
 gulp.task('watch', ['sass'], function () {
     gulp.watch("./sass/*.scss", ['sass']);
-    gulp.watch("./sass/*/*.scss", ['sass']);
 });
 
 gulp.task('sassbuild', function () {
-    return gulp.src("source/sass/*.scss")
-        .pipe(sass())
+    return gulp.src("./sass/*.scss")
+        .pipe(sass({
+            includePaths: ['node_modules/foundation-sites/scss']
+        }))
         .pipe(autoprefixer("last 2 versions", "> 1%", "Explorer 9", "Android 3"))
-        .pipe(gulp.env.production ? csso() : gutil.noop())
-        .pipe(gulp.dest("static/css"))
+        .pipe(gutil.env.prod ? csso() : gutil.noop())
+        .pipe(gulp.dest("./css"))
 });
- 
+
 
 gulp.task('sass', function () {
     return gulp.src('./sass/*.scss')
         .pipe(sass({
-            includePaths:['node_modules/foundation-sites/scss']
+            includePaths: ['node_modules/foundation-sites/scss']
         }))
         .pipe(gulp.dest('./css'));
 });
