4e48eefbd3c4c2d5d6eb7d7fe4deadc2835e2de1
Joachim Jablon <ewjoachim@gmail.com>
2018-05-25 23:28:05 +0200

    sponsors

--- 2018/js/app.js
+++ 2018/js/app.js
@@ -2,33 +2,29 @@
     jQuery(document).ready(function () {
         $('.button-collapse').sideNav();
         $('.parallax').parallax();
-        $('.slick-dots').slick({
+        $('.slick').slick({
             dots: true,
+            adaptiveHeight: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
             prevArrow: $('.prev'),
             nextArrow: $('.next'),
-            infinite: false,
+            infinite: true,
             speed: 300,
-            slidesToShow: 4,
-            slidesToScroll: 4,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             responsive: [
                 {
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
                         infinite: true,
                         dots: true
                     }
                 },
                 {
-                    breakpoint: 600,
-                    settings: {
-                        slidesToShow: 2,
-                        slidesToScroll: 2
-                    }
-                },
-                {
-                    breakpoint: 480,
+                    breakpoint: 620,
                     settings: {
                         slidesToShow: 1,
                         slidesToScroll: 1
