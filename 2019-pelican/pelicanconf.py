bc0df34f9d26e3b4c5105157c430b468667acd98
Joachim Jablon <ewjoachim@gmail.com>
2019-07-07 16:56:04 +0200

    Update scripts

--- 2019-pelican/pelicanconf.py
+++ 2019-pelican/pelicanconf.py
@@ -20,10 +20,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = []
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
@@ -32,4 +29,4 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
--- 2019-pelican/publishconf.py
+++ 2019-pelican/publishconf.py
@@ -12,7 +12,7 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://djangocong.fr/2019'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
