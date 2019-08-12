c11b00b8071c649964108869445020b95455fe06
Joachim Jablon <ewjoachim@gmail.com>
2019-08-13 00:49:48 +0200

    talks

--- 2019-pelican/djangocong_data.py
+++ 2019-pelican/djangocong_data.py
@@ -11,19 +11,95 @@ class Sponsor:
 SPONSORS = []
 
 
+DEFAULT_AVATAR = "https://avatars.githubusercontent.com/{}"
+
+
 @dataclass
 class Talk:
     title: str
     author: str
     description: str
+    time: str
+    avatar: Optional[str] = None
     # Social links
     home_page: Optional[str] = None
     github: Optional[str] = None
     twitter: Optional[str] = None
     linkedin: Optional[str] = None
 
+    @property
+    def image(self):
+        return self.avatar or DEFAULT_AVATAR.format(self.github or "__TBD__")
 
-TALKS = []
+
+TALKS = [
+    Talk(
+        author="Lucien Deleu",
+        title="Un CMS conçu pour la performance écologique",
+        time="Samedi 14 septembre",
+        description="""Description à venir""",
+    ),
+    Talk(
+        author="Sarah Diot-Girard",
+        time="Samedi 14 septembre",
+        title="""De la polysémie en milieu hybride (ou comment faire communiquer 
+            data scientists et développeurs Web)""",
+        description="""Du vocabulaire utilisé à la fois en Data Science et en 
+            développement Web, mais avec des sens radicalement différents. Et du
+            coup les stand-ups deviennent comiques. Et les roadmaps embrouillées.""",
+        github="SdgJlbl",
+    ),
+    Talk(
+        author="Anouk HELLO",
+        time="Samedi 14 septembre",
+        title="""Streaming XLSX from Django to JS: the right way""",
+        description="""Je parlerai de la problématique concernant la place de
+            certaines chaînes traduites: dans l'application client ? Service par
+            l'API ? Dans quels cas et contextes est-ce nécessaire ?
+            Je parlerai de la solution permettant, via un "pipe" JavaScript natif,
+            de streamer le flux HTTP directement dans un fichier sans passer par
+            un objet en mémoire.""",
+    ),
+    Talk(
+        author="Joachim Jablon",
+        time="Samedi 14 septembre",
+        title="""async def django()""",
+        description="""DEP0009 : La proposition d’Andrew Godwin d’ajouter le support
+            asynchrone à Python a été acceptée en Juillet. En quoi consiste-t-elle ?
+            Que va-t-elle changer ? Et comment participer à la transition ?""",
+        github="ewjoachim",
+        twitter="ewjoachim",
+        linkedin="ewjoachim",
+        home_page="https://ewjoach.im",
+    ),
+    Talk(
+        author="Damien Marié",
+        time="Samedi 14 septembre",
+        title="""Et si Django tournait dans votre navigateur, serait-ce la fin de
+            Javascript ?""",
+        description="""Description à venir""",
+    ),
+    Talk(
+        author="Mehdi Raddadi - Eclar",
+        time="Samedi 14 septembre",
+        title="""Déployer Django avec compose""",
+        description="""Explication de la construction d'un squelette de projet déployé
+            avec docker-compose ainsi que le processus de développement associé.
+            Tests en local et sur gitlab-ci puis déploiement.""",
+    ),
+    Talk(
+        author="Arthur Vuillard et Samira Rabaâoui",
+        time="Samedi 14 septembre",
+        title="""Surveiller le bon fonctionnement de son appli Django""",
+        description="""Nous développons des applis web avec Django, mais pas
+            seulement : nous les installons sur un serveur, et devons les faire évoluer
+            au fil du temps.
+            Dans cette présentation, nous allons montrer comment automatiser la
+            surveillance du bon fonctionnement d'une appli Django, et comment être
+            alerté·e·s lors d'une défaillance. Nous présenterons quels sont les
+            éléments à surveiller et comment le faire sans se prendre la tête !""",
+    ),
+]
 
 CONTACT_EMAIL = "j-mad@j-mad.com"
 
